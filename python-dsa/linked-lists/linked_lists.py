f5ae6fd845b3cbc319cc5876be3e2e755fde7157
Mehmet Tarhan <hi@memtarhan.com>
2025-08-22 12:04:01 +0100

    <Python DSA>: LL: Remove

--- python-dsa/linked-lists/linked_lists.py
+++ python-dsa/linked-lists/linked_lists.py
@@ -117,6 +117,24 @@ class LinkedList:
 
         return True
 
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+
+        if index == 0:
+            return self.pop_first()
+
+        if index == self.length - 1:
+            return self.pop()
+
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+
+        return temp
+
 
 my_linked_list = LinkedList()
 
@@ -167,4 +185,9 @@ my_linked_list.set_value(0, 6)
 my_linked_list.print_list()
 
 my_linked_list.insert(0, 9)
+my_linked_list.print_list()
+
+print("removing...")
+my_linked_list.remove(0)
+my_linked_list.remove(0)
 my_linked_list.print_list()
