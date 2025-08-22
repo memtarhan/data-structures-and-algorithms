19a582199d7d36f8badda3836145112deb6f05e9
Mehmet Tarhan <hi@memtarhan.com>
2025-08-22 08:47:02 +0100

    <Python DSA>: LL: Pop

--- python-dsa/linked-lists/linked_lists.py
+++ python-dsa/linked-lists/linked_lists.py
@@ -33,6 +33,27 @@ class LinkedList:
             self.tail = new_node
             self.length += 1
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.head
+            prev = self.head
+
+            while temp.next:
+                prev = temp
+                temp = temp.next
+
+            self.tail = prev
+            self.tail.next = None
+            self.length -= 1
+
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+
+            return temp
+
 
 my_linked_list = LinkedList()
 
@@ -45,4 +66,16 @@ print('Tail:', my_linked_list.tail)
 print('Length:', my_linked_list.length)
 
 print('List...')
+my_linked_list.print_list()
+
+my_linked_list.pop()
+print('List after pop')
+my_linked_list.print_list()
+
+my_linked_list.pop()
+print('List after pop')
+my_linked_list.print_list()
+
+my_linked_list.pop()
+print('List after pop')
 my_linked_list.print_list()
