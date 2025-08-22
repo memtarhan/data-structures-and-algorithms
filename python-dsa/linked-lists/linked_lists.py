5ab9b8a570f76eb7da95a0b5ca54a91a9b047243
Mehmet Tarhan <hi@memtarhan.com>
2025-08-22 12:14:33 +0100

    <Python DSA>: LL: Reverse

--- python-dsa/linked-lists/linked_lists.py
+++ python-dsa/linked-lists/linked_lists.py
@@ -135,6 +135,22 @@ class LinkedList:
 
         return temp
 
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+
+        after = temp.next
+        before = None
+
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+
+
 
 my_linked_list = LinkedList()
 
@@ -190,4 +206,14 @@ my_linked_list.print_list()
 print("removing...")
 my_linked_list.remove(0)
 my_linked_list.remove(0)
+my_linked_list.print_list()
+
+print("---------")
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.print_list()
+
+print("reversing...")
+my_linked_list.reverse()
 my_linked_list.print_list()
