6aa406090c1c657b4fbfe51e816fc718a67c74cc
Mehmet Tarhan <hi@memtarhan.com>
2025-08-23 11:27:04 +0100

    <Python DSA>: Queue: Dequeue

--- python-dsa/stacks-queues/queues.py
+++ python-dsa/stacks-queues/queues.py
@@ -30,6 +30,24 @@ class Queue:
 
         self.length += 1
 
+    def dequeue(self):
+        if self.length == 0:
+            return None
+
+        temp = self.first
+
+        if self.length == 1:
+            self.first = None
+            self.last = None
+
+        else:
+            self.first = self.first.next
+            temp.next = None
+
+        self.length -= 1
+
+        return temp
+
 
 my_queue = Queue(4)
 my_queue.print_queue()
@@ -53,5 +71,26 @@ my_queue.print_queue()
     Queue after enqueue(2):
     1
     2
+
+"""
+
+my_queue = Queue(1)
+my_queue.enqueue(2)
+
+# (2) Items - Returns 2 Node
+print(my_queue.dequeue().value)
+# (1) Item -  Returns 1 Node
+print(my_queue.dequeue().value)
+# (0) Items - Returns None
+print(my_queue.dequeue())
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1
+    2
+    None
 
 """
