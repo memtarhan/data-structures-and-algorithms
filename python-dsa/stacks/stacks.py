4e6c55c5c5d8285f6e3fb7d28e81ca1017f96192
Mehmet Tarhan <hi@memtarhan.com>
2025-08-23 10:55:12 +0100

    <Python DSA>: Stack: Push

--- python-dsa/stacks/stacks.py
+++ python-dsa/stacks/stacks.py
@@ -9,6 +9,24 @@ class Stack:
         self.top = Node(value)
         self.height = 1
 
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        new_node = Node(value)
+
+        if self.height == 0:
+            self.top = new_node
+
+        else:
+            new_node.next = self.top
+            self.top = new_node
+
+        self.height += 1
+
 
 my_stack = Stack(4)
 
@@ -20,5 +38,29 @@ print('Height:', my_stack.height)
     ----------------
     Top: 4
     Height: 1
+
+"""
+
+my_stack = Stack(2)
+
+print('Stack before push(1):')
+my_stack.print_stack()
+
+my_stack.push(1)
+
+print('\nStack after push(1):')
+my_stack.print_stack()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Stack before push(1):
+    2
+
+    Stack after push(1):
+    1
+    2   
 
 """
