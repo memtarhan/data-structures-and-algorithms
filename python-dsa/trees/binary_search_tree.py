5b546fa127e7a64dd0a3b7514c2cdf0c9df9990d
Mehmet Tarhan <hi@memtarhan.com>
2025-08-24 10:54:31 +0100

    <Python DSA>: BST: Constructor

---
+++ python-dsa/trees/binary_search_tree.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+
+my_tree = BinarySearchTree()
+
+print("Root:", my_tree.root)
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Root: None
+
+"""
