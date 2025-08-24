ca3893bd4d3adce4d5d5d0cf69a2ded4fb2112d1
Mehmet Tarhan <hi@memtarhan.com>
2025-08-24 16:05:42 +0100

    <Python DSA>: HT: Get

--- python-dsa/hash-tables/hash_table.py
+++ python-dsa/hash-tables/hash_table.py
@@ -10,6 +10,16 @@ class HashTable:
 
         self.data_map[index].append([key, value])
 
+    def get_item(self, key):
+        index = self.__hash(key)
+
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+            return None
+        return None
+
     def __hash(self, key):
         my_hash = 0
         for letter in key:
@@ -41,5 +51,25 @@ my_hash_table.print_table()
     4 :  [['bolts', 1400], ['washers', 50]]
     5 :  None
     6 :  [['lumber', 70]]
+
+"""
+
+my_hash_table = HashTable()
+
+my_hash_table.set_item('bolts', 1400)
+my_hash_table.set_item('washers', 50)
+
+print('Bolts:', my_hash_table.get_item('bolts'))
+print('Washers:', my_hash_table.get_item('washers'))
+print('Lumber:', my_hash_table.get_item('lumber'))
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Bolts: 1400
+    Washers: 50
+    Lumber: None
 
 """
