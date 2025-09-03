74ef57fa041983c3fa57048d32b6bce185bbba8f
Mehmet Tarhan <hi@memtarhan.com>
2025-09-03 12:35:26 +0100

    <Swift Arcade DSA>: Arrays - Answer 1  - Rotate array to right N times

--- swift-arcade-dsa/2-Arrays.playground/Contents.swift
+++ swift-arcade-dsa/2-Arrays.playground/Contents.swift
@@ -16,12 +16,34 @@ import UIKit
 
  */
 
+/*
+ [3, 8, 9, 7, 6] => [9, 7, 6, 3, 8]
+
+ 3 ---> initial index=0 final index=3
+ 8 ---> initial index=1 final index=4
+ 9 ---> initial index=2 final index=0
+ 7 ---> initial index=3 final index=2
+ 6 ---> initial index=4 final index=2
+ 
+ */
 func solution(A: [Int], K: Int) -> [Int] {
-    return []
+    var newArr = A
+    var count = A.count
+    var actualRotationCount = K % count
+
+    for index in 0 ..< count {
+        var newIndex = index + actualRotationCount
+        if newIndex > count - 1 {
+            newIndex = newIndex - count
+        }
+        newArr[newIndex] = A[index]
+    }
+
+    return newArr
 }
 
 solution(A: [1, 2, 3, 4, 5], K: 1) // 5 1 2 3 4
 solution(A: [1, 2, 3, 4, 5], K: 2) // 4 5 1 2 3
 solution(A: [1, 2, 3, 4, 5], K: 3) // 3 4 5 1 2
 
-solution(A: [3, 8, 9, 7, 6], K: 3) // [9, 7, 6, 3, 8]
+solution(A: [3, 8, 9, 7, 6], K: 13) // [9, 7, 6, 3, 8]
