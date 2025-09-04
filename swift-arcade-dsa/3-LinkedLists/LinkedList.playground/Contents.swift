ca84ab1487ed32055205e3a8126279d8ab31af9a
Mehmet Tarhan <hi@memtarhan.com>
2025-09-04 11:53:40 +0100

    <Swift Arcade DSA>: LinkedList - add back and get last

--- swift-arcade-dsa/3-LinkedLists/LinkedList.playground/Contents.swift
+++ swift-arcade-dsa/3-LinkedLists/LinkedList.playground/Contents.swift
@@ -20,8 +20,39 @@ class LinkedList {
         head = newNode
     }
 
+    // O(1)
     func getFirst() -> Node? {
         return head
+    }
+
+    // O(n)
+    func addBack(_ data: Int) {
+        let newNode = Node(data)
+
+        guard let head else {
+            self.head = newNode
+            return
+        }
+
+        var temp = head
+
+        while temp.next != nil {
+            temp = temp.next!
+        }
+
+        temp.next = newNode
+    }
+
+    // O(n)
+    func getLast() -> Node? {
+        if head == nil { return nil }
+
+        var temp = head!
+        while temp.next != nil {
+            temp = temp.next!
+        }
+
+        return temp
     }
 
     func printLinkedList() {
@@ -51,3 +82,14 @@ linkedList.addFront(2)
 linkedList.addFront(1)
 
 linkedList.printLinkedList()
+
+linkedList.addBack(4)
+linkedList.printLinkedList()
+
+if let first = linkedList.getFirst() {
+    print(first.data)
+}
+
+if let last = linkedList.getLast() {
+    print(last.data)
+}
