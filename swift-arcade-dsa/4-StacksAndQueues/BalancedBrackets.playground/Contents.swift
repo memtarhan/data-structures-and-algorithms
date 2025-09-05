b83bf7834ad156586cbabf67e0eb7a3450877c6c
Mehmet Tarhan <hi@memtarhan.com>
2025-09-05 13:56:05 +0100

    <Swift Arcade DSA>: Stacks and Queues - Answer - Balanced Brackets

--- swift-arcade-dsa/4-StacksAndQueues/BalancedBrackets.playground/Contents.swift
+++ swift-arcade-dsa/4-StacksAndQueues/BalancedBrackets.playground/Contents.swift
@@ -19,7 +19,33 @@ import UIKit
  */
 
 func isBalanced(s: String) -> String { // "{[()]}"
-    return "NO"
+    var st = [Character]()
+
+    for c in s {
+        switch c {
+        case "{", "(", "[":
+            st.append(c)
+        case "}":
+            if st.isEmpty || (st.last != "{") {
+                return "NO"
+            }
+            st.popLast()
+        case ")":
+            if st.isEmpty || (st.last != "(") {
+                return "NO"
+            }
+            st.popLast()
+        case "]":
+            if st.isEmpty || (st.last != "[") {
+                return "NO"
+            }
+            st.popLast()
+        default:
+            print("breaking \(c)")
+        }
+    }
+
+    return st.isEmpty ? "YES" : "NO"
 }
 
 isBalanced(s: "{[()]}") // Yes
