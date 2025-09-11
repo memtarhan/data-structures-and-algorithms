217005fb0cabceaf1a453ee948950e34c8d2efb1
Mehmet Tarhan <hi@memtarhan.com>
2025-09-11 11:56:05 +0100

    <HackerRank>: Algorithms - Implementation - Number Line Jump

--- hackerrank/Algorithms/Implementation/NumberLineJump.playground/Contents.swift
+++ hackerrank/Algorithms/Implementation/NumberLineJump.playground/Contents.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 /*
  * Complete the 'kangaroo' function below.
  *
@@ -13,12 +11,10 @@ import Foundation
 
 func kangaroo(x1: Int, v1: Int, x2: Int, v2: Int) -> String {
     let neverMeets = (x2 > x1 && v2 > v1) || (x1 > x2 && v1 > v2)
-    print("neverMeets: \(neverMeets)")
+    let neverMeets2 = !((x2 > x1 && v1 > v2) || (x1 > x2 && v2 > v1))
+    let neverMeets3 = v1 == v2
 
-    let neverMeet2 = (x2 - x1 > v2 - v1) || (x1 - x2 > v1 - v2)
-    print("neverMeet2: \(neverMeet2)")
-
-    if neverMeets {
+    if neverMeets || neverMeets2 || neverMeets {
         return "NO"
     }
 
@@ -30,11 +26,15 @@ func kangaroo(x1: Int, v1: Int, x2: Int, v2: Int) -> String {
         current2 += v2
 
         print("x1: \(current1) - x2: \(current2)")
-        
+
         counter += 1
 
         if current1 == current2 {
             return "YES"
+        }
+
+        if current1 > current2 {
+            return "NO"
         }
     }
 
@@ -43,11 +43,5 @@ func kangaroo(x1: Int, v1: Int, x2: Int, v2: Int) -> String {
 
 // kangaroo(x1: 2, v1: 1, x2: 1, v2: 2)
 // kangaroo(x1: 0, v1: 3, x2: 4, v2: 2)
-// kangaroo(x1: 0, v1: 2, x2: 5, v2: 3)
-
- kangaroo(x1: 4523, v1: 8092, x2: 9419, v2: 8076)
-//kangaroo(x1: 21, v1: 6, x2: 47, v2: 3)
-//kangaroo(x1: 28, v1: 8, x2: 96, v2: 2)
-//kangaroo(x1: 1817, v1: 9931, x2: 8417, v2: 190)
-
-
+// kangaroo(x1: 45, v1: 7, x2: 56, v2: 3)
+kangaroo(x1: 43, v1: 2, x2: 70, v2: 2)
