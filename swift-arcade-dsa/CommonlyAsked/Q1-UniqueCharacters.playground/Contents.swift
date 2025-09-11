f2d651aec032456eb78c20730268e81b31e0c448
Mehmet Tarhan <hi@memtarhan.com>
2025-09-11 09:11:38 +0100

    <Swift Arcade DSA>: Questions - Unique Characters (Answer)

--- swift-arcade-dsa/CommonlyAsked/Q1-UniqueCharacters.playground/Contents.swift
+++ swift-arcade-dsa/CommonlyAsked/Q1-UniqueCharacters.playground/Contents.swift
@@ -1,20 +1,46 @@
 import UIKit
 
 /*
- _   _      _                ___ _
-| | | |_ _ (_)__ _ _  _ ___ / __| |_  __ _ _ _ ___
-| |_| | ' \| / _` | || / -_) (__| ' \/ _` | '_(_-<
- \___/|_||_|_\__, |\_,_\___|\___|_||_\__,_|_| /__/
-                |_|
+  _   _      _                ___ _
+ | | | |_ _ (_)__ _ _  _ ___ / __| |_  __ _ _ _ ___
+ | |_| | ' \| / _` | || / -_) (__| ' \/ _` | '_(_-<
+  \___/|_||_|_\__, |\_,_\___|\___|_||_\__,_|_| /__/
+                 |_|
 
- Challenge: Given a string, see if you can detect whether it contains only unique chars.
- 
+  Challenge: Given a string, see if you can detect whether it contains only unique chars.
+
+  */
+
+/*
+ Solution:
+
+  There are lots of ways we could solve this:
+       - HashMap where we store every character and a boolean on whether found
+       - An array that counts the number of times a character occurs
  */
 
-func isUnique(_ text: String) -> Bool {
+func isUniqueWithDictionary(_ text: String) -> Bool {
+    var foundChars = [Character: Bool]()
+    let chars = Array(text)
+
+    for c in chars {
+        if foundChars[c] != nil { // Already found
+            return false
+        }
+        foundChars[c] = true
+    }
     return true
 }
 
-isUnique("ab")
-isUnique("aa")
-isUnique("abcdefghijklmnopqrstuvwxyz")
+isUniqueWithDictionary("ab")
+isUniqueWithDictionary("aa")
+isUniqueWithDictionary("abcdefghijklmnopqrstuvwxyz")
+
+func isUniqueWithSet(_ text: String) -> Bool {
+    var set = Set(text)
+    return set.count == text.count
+}
+
+isUniqueWithSet("ab")
+isUniqueWithSet("aa")
+isUniqueWithSet("abcdefghijklmnopqrstuvwxyz")
