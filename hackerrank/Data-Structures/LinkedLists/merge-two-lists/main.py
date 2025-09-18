3956e4f90b7e2ade360d2d265e234953010c8afe
Mehmet Tarhan <hi@memtarhan.com>
2025-09-18 19:17:12 +0100

    <HackerRank>: Data Structures - Linked Lists - Merge two linked lists

--- hackerrank/Data-Structures/LinkedLists/merge-two-lists/main.py
+++ hackerrank/Data-Structures/LinkedLists/merge-two-lists/main.py
@@ -1,3 +1,21 @@
+"""
+
+Given pointers to the heads of two sorted linked lists, merge them into a single, sorted linked list. Either head pointer may be null meaning that the corresponding list is empty.
+Example
+ headA refers to 1 -> 3 -> 7 -> None
+ headB refers to 1 -> 2 -> None
+The new list is
+Function Description
+Complete the mergeLists function in the editor below.
+mergeLists has the following parameters:
+SinglyLinkedListNode pointer headA: a reference to the head of a list
+SinglyLinkedListNode pointer headB: a reference to the head of a list
+Returns
+SinglyLinkedListNode pointer: a reference to the head of the merged list
+
+"""
+
+
 # Complete the mergeLists function below.
 
 #
@@ -26,33 +44,62 @@ def print_linked_list(node):
 
 
 def merge_lists(head1, head2):
-    current1 = head1
-    current2 = head2
+    pointer1 = head1
+    pointer2 = head2
 
     head = None
-    current = head
+    current = None
 
-    while current1 is not None or current2 is not None:
-        if current1 is not None and current2 is not None:
-            if current1.data >= current2.data:
-                current = current1
-                current1 = current1.next
+    while pointer1 is not None or pointer2 is not None:
+
+        if pointer1 is not None and pointer2 is not None:
+            # print(f"pointer 1: {pointer1.data} ---- pointer 2: {pointer2.data}")
+
+            if pointer1.data <= pointer2.data:
+                if head is None:
+                    head = pointer1
+                elif current is None:
+                    current = pointer1
+                    head.next = current
+                else:
+                    current.next = pointer1
+                    current = current.next
+                pointer1 = pointer1.next
             else:
-                current = current2
-                current2 = current2.next
-
-        elif current1 is not None:
-            current = current1
-            current1 = current1.next
+                if head is None:
+                    head = pointer2
+                elif current is None:
+                    current = pointer2
+                    head.next = current
+                else:
+                    current.next = pointer2
+                    current = current.next
+                pointer2 = pointer2.next
 
         else:
-            current = current2
-            current2 = current2.next
+            if pointer1:
+                # print(f"pointer 1: {pointer1.data} ---- ")
+                if head is None:
+                    head = pointer1
+                elif current is None:
+                    current = pointer1
+                    head.next = current
+                else:
+                    current.next = pointer1
+                    current = current.next
+                pointer1 = pointer1.next
 
-        if head is None:
-            head = current
-        else:
-            current.next = current
+            elif pointer2:
+                # print(f" ---- pointer 2: {pointer2.data}")
+                if head is None:
+                    head = pointer2
+                elif current is None:
+                    current = pointer2
+                    head.next = current
+                else:
+                    current.next = pointer2
+                    current = current.next
+                pointer2 = pointer2.next
 
     return head
 
