0a4970df88de49359bbf0f612d008d8831920441
Mehmet Tarhan <hi@memtarhan.com>
2025-09-23 16:09:07 +0100

    <HackerRank>: Data Structures - Stacks - Maximum element (incomplete)

--- hackerrank/Data-Structures/Stacks/maximum-element/main.py
+++ hackerrank/Data-Structures/Stacks/maximum-element/main.py
@@ -20,6 +20,8 @@ def get_max(operations):
     max_stack = Stack()
     stack = Stack()
 
+    max_data = []
+
     for operation in operations:
         parts = operation.split(" ")
         operation_type = int(parts[0])
@@ -47,9 +49,25 @@ def get_max(operations):
 
         elif operation_type == 3:
             # get max
-            print(max_stack.peek())
+            max_value = max_stack.peek()
+            if max_value:
+                max_data.append(max_value)
+
+    return max_data
 
 
 if __name__ == '__main__':
-    ops = ['1 97', '2', '1 20', '2', '1 26', '1 20', '2', '3', '1 91', '3']
-    get_max(ops)
+    # ops = ['1 97', '2', '1 20', '2', '1 26', '1 20', '2', '3', '1 91', '3']
+    # get_max(ops)
+
+    n = int(input().strip())
+
+    ops = []
+
+    for _ in range(n):
+        ops_item = input()
+        ops.append(ops_item)
+
+    res = get_max(ops)
+
+    print(res)
